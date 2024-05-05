57ecf61d6f5f08dcdead40651463813a30028df1
nukiexl <129978663+nukiexl@users.noreply.github.com>
2024-05-05 13:24:24 +0300

    Проект создан

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            int result = 0;
+            char operation;
+
+            Console.Write("Введите первое число: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите операцию (+, -, *, /): ");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                        result = num1 / num2;
+                    break;
+            }
+
+            Console.WriteLine("Результат: " + result);
+        }
+    }
+}
